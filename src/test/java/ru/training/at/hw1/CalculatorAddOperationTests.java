1e48468bd3ce27645fbacbcc05aa10565f64ad9b
Flowina <bilobik@mail.ru>
2021-05-06 13:55:33 +0300

    HW 1

---
+++ src/test/java/ru/training/at/hw1/CalculatorAddOperationTests.java
@@ -0,0 +1,29 @@
+package ru.training.at.hw1;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class CalculatorAddOperationTests extends  BaseCalculatorTests {
+    @DataProvider
+    public Object[][] data() {
+        return new Object[][]{
+                {0L, 10L, 10L},
+                {5L, 10L, 15L},
+                {-5L, 1L, -4L},
+        };
+    }
+
+    @Test(dataProvider = "data",
+        groups = {Tags.ADD})
+    public void addIntegersTest(long a, long b, long expected) {
+        long sum = calculator.sum(a, b);
+        Assert.assertEquals(sum, expected, "wrong sum of integers");
+    }
+
+    @Test (expectedExceptions = Exception.class,
+            groups = {Tags.ADD})
+    public void addMaxLongTest() {
+        long sum = calculator.sum(Long.MAX_VALUE, Long.MAX_VALUE);
+    }
+}
---
+++ src/test/java/ru/training/at/hw1/Tags.java
@@ -0,0 +1,8 @@
+package ru.training.at.hw1;
+
+public interface Tags {
+    String ADD = "add";
+    String SUBTRACT = "subtract";
+    String MULTIPLY = "multiply";
+    String DIVIDE = "divide";
+}

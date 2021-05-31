b09331f82b50dc2a347361bb701ab58d534a9deb
Flowina <bilobik@mail.ru>
2021-05-31 17:29:41 +0300

    HW7. Selenium Grid

---
+++ src/test/java/ru/training/at/hw7/pages/JdiTestingPage.java
@@ -0,0 +1,31 @@
+package ru.training.at.hw7.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public abstract class JdiTestingPage {
+    protected static final int WAIT_TIMEOUT_SECONDS = 10;
+    protected WebDriver driver;
+
+    @FindBy(id = "user-name")
+    private WebElement userNameElement;
+
+    protected JdiTestingPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    protected abstract String getUrl();
+
+    public void openPage() {
+        driver.navigate().to(getUrl());
+    }
+
+    public String getTitle() {
+        return driver.getTitle();
+    }
+
+    public String getUserName() {
+        return userNameElement.getText();
+    }
+}
